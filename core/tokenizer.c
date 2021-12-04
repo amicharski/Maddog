bdc8940c2e412f0964f1f36fd001ecb9eca7625d
Alex Micharski <amicharski@outlook.com>
2021-12-04 16:27:54 -0500

    more work on the tokenizer

--- core/tokenizer.c
+++ core/tokenizer.c
@@ -31,7 +31,7 @@ bool is_current_word(const char* word){
             return false;
         }
     }
-    i += strlen(word); //-1;
+    i += strlen(word);
     return true;
 }
 
@@ -62,27 +62,33 @@ bool is_alphanumeric(char ch){
 bool consume_binary(){
     if(!is_current_word("0b")) return false;
     do {} while(is_bin(source[i++]));
-    if(is_alphanumeric(source[i++])) return false;
+    if(is_alphanumeric(source[--i])) return false;
     return true;
 }
 
 bool consume_octal(){
     if(!is_current_word("0c")) return false;
     do {} while(is_oct(source[i++]));
-    if(is_alphanumeric(source[i++])) return false;
+    if(is_alphanumeric(source[--i])) return false;
     return true;
 }
 
 bool consume_hex(){
     if(!is_current_word("0x")) return false;
     do {} while(is_hex(source[i++]));
-    if(is_alphanumeric(source[i++])) return false;
+    if(is_alphanumeric(source[--i])) return false;
     return true;
 }
 
 bool consume_int(){
     do {} while(is_digit(source[i++]));
-    if(is_alpha(source[i++])) return false;
+    if(is_alpha(source[--i])) return false;
+    return true;
+}
+
+bool consume_identifier(){
+    do {} while(is_alphanumeric(source[i++]));
+    --i;
     return true;
 }
 
@@ -92,7 +98,7 @@ char* tokenize(char* sourceCode){
     do {
         char ch = sourceCode[i];
         printf("\nch: %c\n", ch);
-        switch(ch){
+        switch(ch) {
             case '(':
                 tokens[token_counter++] = LEFT_PAREN;
                 print_to_shell("LEFT PARENTHESIS");
@@ -118,13 +124,157 @@ char* tokenize(char* sourceCode){
                 print_to_shell("RIGHT BRACE");
                 break;
             case '<':
-                tokens[token_counter++] = LEFT_ANGLE;
-                print_to_shell("LEFT ANGLE");
-                break;
+                if (peek_next() == '=') {
+                    tokens[token_counter++] = COND_LTEQU;
+                    print_to_shell("LESS THAN OR EQUAL TO");
+                    break;
+                } else if(strcmp(peek_range(2), "<=") == 0){
+                    tokens[token_counter++] = ASSIGN_SHIFTLTEQU;
+                    print_to_shell("SHIFT LEFT EQU");
+                    break;
+                } else if(peek_next() == '<'){
+                    tokens[token_counter++] = SHIFTLT;
+                    print_to_shell("SHIFT LEFT");
+                    break;
+                } else if(strcmp(peek_range(3), "!+>") == 0){
+                    tokens[token_counter++] = XNOR;
+                    print_to_shell("XNOR");
+                    break;
+                } else if(strcmp(peek_range(2), "+>") == 0){
+                    tokens[token_counter++] = XOR;
+                    print_to_shell("XOR");
+                    break;
+                } else {
+                    tokens[token_counter++] = LEFT_ANGLE;
+                    print_to_shell("LEFT ANGLE");
+                    break;
+                }
             case '>':
-                tokens[token_counter++] = RIGHT_ANGLE;
-                print_to_shell("RIGHT ANGLE");
+                if (peek_next() == '=') {
+                    tokens[token_counter++] = COND_GTEQU;
+                    print_to_shell("GREATER THAN OR EQUAL TO");
+                    break;
+                } else if(strcmp(peek_range(2), ">=") == 0){
+                    tokens[token_counter++] = ASSIGN_SHIFTRTEQU;
+                    print_to_shell("SHIFT RIGHT EQU");
+                    break;
+                } else if(peek_next() == '>'){
+                    tokens[token_counter++] = SHIFTRT;
+                    print_to_shell("SHIFT RIGHT");
+                    break;
+                } else {
+                    tokens[token_counter++] = RIGHT_ANGLE;
+                    print_to_shell("RIGHT ANGLE");
+                    break;
+                }
+            case '.':
+                tokens[token_counter++] = DOT;
+                print_to_shell("DOT");
                 break;
+            case ';':
+                tokens[token_counter++] = END_OF_LINE;
+                print_to_shell("END OF LINE");
+                break;
+            case '=':
+                if(peek_next() == '='){
+                    tokens[token_counter++] = COND_EQU;
+                    print_to_shell("CONDITIONAL EQUAL SIGN");
+                    break;
+                } else {
+                    tokens[token_counter++] = ASSIGN_EQU;
+                    print_to_shell("ASSIGN EQUAL SIGN");
+                    break;
+                }
+            case '+':
+                if(peek_next() == '=') {
+                    tokens[token_counter++] = ASSIGN_ADDEQU;
+                    print_to_shell("ADD EQUAL SIGN");
+                    break;
+                } else if(peek_next() == '+'){
+                    tokens[token_counter++] = INCREMENT;
+                    print_to_shell("INCREMENT");
+                    break;
+                } else {
+                    tokens[token_counter++] = ADD;
+                    print_to_shell("ADD");
+                    break;
+                }
+            case '-':
+                if(peek_next() == '='){
+                    tokens[token_counter++] = ASSIGN_SUBEQU;
+                    print_to_shell("SUB EQUAL SIGN");
+                    break;
+                } else if(peek_next() == '-'){
+                    tokens[token_counter++] = DECREMENT;
+                    print_to_shell("DECREMENT");
+                    break;
+                } else {
+                    tokens[token_counter++] = SUB;
+                    print_to_shell("SUB");
+                    break;
+                }
+            case '*':
+                if(peek_next() == '*'){
+                    tokens[token_counter++] = ASSIGN_MULEQU;
+                    print_to_shell("MULT EQUAL SIGN");
+                    break;
+                } else if(peek_next() == '*'){
+                    tokens[token_counter++] = MULT_TWO;
+                    print_to_shell("MULT TWO");
+                    break;
+                } else {
+                    tokens[token_counter++] = MULT;
+                    print_to_shell("MULT");
+                    break;
+                }
+            case '/':
+                if(peek_next() == '='){
+                    tokens[token_counter++] = ASSIGN_DIVEQU;
+                    print_to_shell("DIV EQUAL SIGN");
+                    break;
+                } else if(peek_next() == '/'){
+                    tokens[token_counter++] = DIV_TWO;
+                    print_to_shell("DIV TWO");]
+                    break;
+                } else {
+                    tokens[token_counter++] = DIV;
+                    print_to_shell("DIV");
+                    break;
+                }
+            case '%':
+                if(peek_next() == '='){
+                    tokens[token_counter++] = ASSIGN_MODEQU;
+                    print_to_shell("MOD EQUAL SIGN");
+                    break;
+                } else if(peek_next() == '%'){
+                    tokens[token_counter++] = MOD_TWO;
+                    print_to_shell("MOD TWO");
+                    break;
+                } else {
+                    tokens[token_counter++] = MOD;
+                    print_to_shell("MOD");
+                    break;
+                }
+            case '^':
+                if(peek_next() == '='){
+                    tokens[token_counter++] = ASSIGN_EXPEQU;
+                    print_to_shell("EXP EQUAL SIGN");
+                    break;
+                } else if(peek_next() == '^'){
+                    tokens[token_counter++] = EXP_TWO;
+                    print_to_shell("EXP TWO");
+                    break;
+                } else {
+                    tokens[token_counter++] = EXP;
+                    print_to_shell("EXP");
+                    break;
+                }
+            case '?':
+                tokens[token_counter++] = TERNQ;
+                print_to_shell("TERNARY QUESTION MARK");
+                break;
+            case '!':
+
             default:
                 if(is_current_word("import")){
                     tokens[token_counter++] = IMPORT;
@@ -165,6 +315,13 @@ char* tokenize(char* sourceCode){
                 } else if(is_current_word("INSTANCE")){
                     tokens[token_counter++] = INSTANCE;
                     print_to_shell("INSTANCE");
+                } else if(is_current_word("TRUE")
+                || is_current_word("FALSE")) {
+                    tokens[token_counter++] = BOOL_LITERAL;
+                    print_to_shell("BOOL LITERAL");
+                } else if(is_current_word("NULL")){
+                    tokens[token_counter++] = NULL_TOKEN;
+                    print_to_shell("NULL TOKEN");
                 } else if(is_digit(ch)){
                     if(ch == '0'){
                         if(peek_next() == 'b'){
@@ -173,6 +330,7 @@ char* tokenize(char* sourceCode){
                             } else {
                                 tokens[token_counter++] = BIN_LITERAL;
                                 print_to_shell("BIN LITERAL");
+                                continue;
                             }
                         } else if(peek_next() == 'c'){
                             if(!consume_octal()) {
@@ -180,6 +338,7 @@ char* tokenize(char* sourceCode){
                             } else {
                                 tokens[token_counter++] = OCT_LITERAL;
                                 print_to_shell("OCT LITERAL");
+                                continue;
                             }
                         } else if(peek_next() == 'x'){
                             if(!consume_hex()) {
@@ -187,13 +346,23 @@ char* tokenize(char* sourceCode){
                             } else {
                                 tokens[token_counter++] = HEX_LITERAL;
                                 print_to_shell("HEX LITERAL");
+                                continue;
                             }
                         }
                     }
-                    if(is_digit(ch)){
-                        consume_int();
+                    if(consume_int()){
+                        tokens[token_counter++] = INT_LITERAL;
+                        print_to_shell("INT LITERAL");
+                        continue;
                     }
+                } else if(is_alpha(ch)){
+                    consume_identifier();
+                    tokens[token_counter++] = IDENTIFIER;
+                    print_to_shell("IDENTIFIER");
+                    continue;
                 }
         }
     } while (sourceCode[i++] != '\0');
+    tokens[token_counter++] = EOF_TOKEN;
+    print_to_shell("EOF");
 }
